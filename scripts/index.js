02577b24cb8e7faa204fc5fdec99aa6d6d980306
mscopa <maurocopa20@gmail.com>
2024-01-25 12:38:38 -0300

    week03 team activities

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,34 @@
 const menu = document.getElementById('menu');
 const nav = document.querySelector('nav');
+const modeButton = document.querySelector("#darkBtn");
+const main = document.querySelector("main");
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
 menu.addEventListener('click', () => {
     nav.classList.toggle('open');
     menu.classList.toggle('open');
 })
 
-const modeButton = document.querySelector("#darkBtn");
-const main = document.querySelector("main");
-
 modeButton.addEventListener("click", () => {
-	if (modeButton.getAttribute("src") == "images/dark.svg") {
+	if (modeButton.getAttribute("src") == "images/dark.webp") {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-		modeButton.setAttribute("src", "images/light.svg");
+		modeButton.setAttribute("src", "images/light.webp");
 		modeButton.alt = "light-mode";
 	} else {
         main.style.background = "#eee";
 		main.style.color = "#000";
-		modeButton.setAttribute("src", "images/dark.svg");
+		modeButton.setAttribute("src", "images/dark.webp");
 		modeButton.alt = "dark-mode";
 	}
 });
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
