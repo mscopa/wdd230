e10eb25e9d89b564f33e9c7796410aa893f8554e
mscopa <maurocopa20@gmail.com>
2024-02-22 02:44:46 -0300

    applying the milestone feedbacks for chamber site

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,29 +1,2 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = new Date(document.lastModified);
-
-function getLatestVisit() {
-    return JSON.parse(window.localStorage.getItem("latestVisit"));
-}
-
-const msToDays = 86400000;
-const currentDate = Date.now();
-const latestVisitMessage = document.getElementById("latestVisit");
-const latestVisit = getLatestVisit() || 0;
-if (latestVisit === 0) {
-    latestVisitMessage.textContent = "Welcome! Let us know if you have any questions."
-    window.localStorage.setItem("latestVisit", currentDate)
-}
-else if (currentDate - getLatestVisit() < msToDays) {
-    latestVisitMessage.textContent = "Back so soon! Awesome!"
-    window.localStorage.setItem("latestVisit", currentDate)
-}
-else {
-    if ((latestVisit - getLatestVisit()) / msToDays < 1) {
-        latestVisitMessage.textContent = `You last visited ${((latestVisit - getLatestVisit()) / msToDays).toFixed(0)} day ago.`
-    }
-    else {
-        latestVisitMessage.textContent = `You last visited ${((latestVisit - getLatestVisit()) / msToDays).toFixed(0)} days ago.`
-    }
-    window.localStorage.setItem("latestVisit", currentDate)
-}
-
